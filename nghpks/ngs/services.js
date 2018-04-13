facca1c6135a2b39be0a499b6d9c5077071c2110
Stefano L <stefan22@users.noreply.github.com>
2018-04-13 23:45:05 +0100

    Create services.js

---
+++ nghpks/ngs/services.js
@@ -0,0 +1,106 @@
+/*
+    another example of a service between two controllers to extract function's methods
+    and accessing them without $scope just with 'var name = this' to access the global scope
+    for a method, function, array, variable or property.
+*/
+
+
+var myApp = angular.module('myApp', []);
+
+//AddToListController controller
+var AddToListController = myApp.controller('AddToListController', AddToListController);
+
+//NeedListController controller
+var NeedListController = myApp.controller('NeedListController', NeedListController);
+
+// Service list
+var ServiceList = myApp.service('ServiceList', ServiceList);
+
+AddToListController.$inject = ['ServiceList'];
+
+function AddToListController(ServiceList) {
+    var addlist = this;
+    addlist.addItem = function() {
+        console.log(addlist.itemName, addlist.itemQty);
+        //add to list
+        ServiceList.addToList(addlist.itemName,addlist.itemQty);
+    };
+}
+
+function NeedListController(ServiceList) {
+    var needlist = this;
+    //items
+    needlist.items = ServiceList.showTime();
+
+}
+
+
+function ServiceList() {
+    var service = this;
+    var items = [];
+
+    service.addToList = function(name,quantity) {
+        var item = {
+            name: name,
+            quantity: quantity
+        };
+        items.push(item);
+        console.log(items);
+        console.log(item);
+    }
+
+    service.showTime = function() {
+        console.log(items);
+        return items;
+    }
+
+}
+
+
+//html
+
+/*
+
+  <body>   
+  <div class="shop">
+    <h1>Shopping List</h1>
+    <div class="addlist" ng-controller="AddToListController as addlist">
+        <p>Enter items to your shopping list:</p>
+        <input type="text" id="item" class="inputfield" ng-model="addlist.itemName" placeholder="enter item to list" />
+
+        <p>Enter the quantity:</p>
+        <input type="number" id="qty" class="inputfield" ng-model="addlist.itemQty" placeholder="enter quantity" />
+
+        <button ng-click="addlist.addItem();">Add to List</button>
+    <br/>
+    <br/>
+    <br/>
+
+    {{addlist.itemName}} <br/><br/>
+
+    {{addlist.itemQty}}
+
+    </div>
+    <!-- endof addlist -->
+
+    <div class="needlist" ng-controller="NeedListController as needlist">
+        <h2>List Items:</h2>
+        <!-- list items here -->
+        
+        <ul>
+            <li ng-repeat="item in needlist.items">{{item.name}}, qty: {{item.quantity}}</li>
+        </ul>
+
+    </div>
+
+  </div>
+  
+  
+</body>
+
+
+
+//endof html
+
+*/
+
