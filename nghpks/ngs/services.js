e48da28dc87cd0bbbabef80eca11ef4d056401d0
Stefano L <stefan22@users.noreply.github.com>
2018-04-14 20:04:06 +0100

    Update services.js

--- nghpks/ngs/services.js
+++ nghpks/ngs/services.js
@@ -2,6 +2,8 @@
     another example of a service between two controllers to extract function's methods
     and accessing them without $scope just with 'var name = this' to access the global scope
     for a method, function, array, variable or property.
+    
+    in bottom the same shopping list w/add and remove but with one controller and no scope.
 */
 
 
@@ -118,4 +120,78 @@ function ServiceList() {
 //endof html
 
 */
+
+/*
+        --------------------- endof 2 controllers and 1 service list add/remove--------------------------
+*/
+
+
+/*
+
+    add/remove items but with just one controller and no service or scope
+*/
+
+
+//AddToListController controller
+var ShoppingListController1 = myApp.controller('ShoppingListController1', ShoppingListController1);
+
+function ShoppingListController1() {
+    var list1 = this;
+    var items = [];
+    
+    list1.addItem = function(itemName,itemQuantity) {
+        console.log(itemName);
+        var item = {
+            name:itemName,
+            quantity:itemQuantity
+        };
+        items.push(item);
+        console.log(items);
+        return list1.items;
+    };
+
+    list1.removeItem = function(index) {
+        items.splice(index,1);
+    }
+
+    list1.items = items;
+    
+}
+
+
+
+
+/*
+    almost same html but using just one controller
+    
+    
+    <div class="addlist" ng-controller="ShoppingListController1 as list1">
+        <h3>Shopping list 1</h3>
+
+        <input type="text" id="item" class="inputfield" ng-model="list1.itemName" placeholder="item name" />
+
+        <input type="number" id="qty" class="inputfield" ng-model="list1.itemQuantity" placeholder="quantity" />
+
+        <button ng-click="list1.addItem(list1.itemName,list1.itemQuantity);">Add Item</button>
+        <br/>
+        <br/>
+        <br/>
+        {{list1.itemName}}  {{list1.itemQuantity}}
+
+        <ul>
+            <li ng-repeat="item in list1.items">
+                {{item.name}}, qty: {{item.quantity}}
+            <button ng-click="list1.removeItem($index);">Remove item</button>
+            </li>
+
+        </ul>
+
+    </div>
+
+
+
+*/
+
+
+
 
