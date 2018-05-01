570a1c3d849107e4ac0b7d144f4aec18ccf0ae81
Stefano L <stefan22@users.noreply.github.com>
2018-05-01 23:50:50 +0100

    app.js controller, service

--- nghpks/app.js
+++ nghpks/app.js
@@ -1,1 +1,56 @@
 
+var myApp = angular.module('myApp', []);
+
+var ShoppingListController1 = myApp
+.controller('ShoppingListController1', ShoppingListController1);
+
+var ShoppingListService = myApp.service('ShoppingListService', ShoppingListService);
+
+ShoppingListController1.$inject = ['ShoppingListService'];
+
+
+function ShoppingListController1(ShoppingListService) {
+
+	var list1 = this;
+
+	// list1 addItem
+	list1.addItem = function() {
+		ShoppingListService.addItem(list1.itemName,list1.itemQty);
+		
+	};
+
+	list1.shoppingList = ShoppingListService.showList();
+
+}
+
+
+function ShoppingListService() {
+
+	var service = this;
+
+	var shoppingList = [];
+
+	service.addItem = function(itemName,itemQty) {
+		console.log(itemName,itemQty);
+		var item = {
+			name: itemName,
+			qty: itemQty
+		};
+
+		console.log(item);
+		// add to array
+		shoppingList.push(item);
+		console.log(shoppingList);
+
+	};
+
+	service.showList = function() {
+		return shoppingList;
+	};
+
+	
+		
+
+
+
+}
