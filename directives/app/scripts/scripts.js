4d65aeaf9295b26039b04dd7d8d6c390e50f6ba1
Stefano <stefano.lissiaeu@gmail.com>
2018-06-03 23:58:38 +0100

    service

--- directives/app/scripts/scripts.js
+++ directives/app/scripts/scripts.js
@@ -1,76 +1,95 @@
-var shopApp = angular.module('shopApp', []);
+(function() {
 
-var ShopController1 = shopApp.controller('ShopController1', ShopController1);
+    var shopApp = angular.module('shopApp', []);
 
+    var ShopController1 = shopApp.controller('ShopController1', ShopController1);
+    var ShopController2 = shopApp.controller('ShopController2', ShopController2);
 
-function ShopController1() {
-	var shop1 = this;
-	var list = [];
+    var ShoppingListService = shopApp.service('ShoppingListService',ShoppingListService);
 
-	shop1.addItem = function(name,qty) {
-		var num;
-		var item = {};
-		item.name = name;
-		item.qty = qty;
-		num = String(item.qty);
-		//rounded
-		if(num.length > 1) {
-			item.qty = item.qty;
-		}else
-		if(num.length == 1) {
-			item.qty = String(0) + item.qty;
-		}
-
-		list.push(item);
-		return list;
-	};
-
-	shop1.list = list;
-
-	shop1.removeItem = function($index) {
-		list.splice($index,1);
-		return list;
-	};
+    //ShopController1.$inject = ["ShoppingListService"];
 
 
+    function ShopController1() {
+        var shop = this;
+        var list = [];
+        //var shoplist = ShoppingListFactory();
 
-}//shop1 fn
+        shop.addItem = function(name, qty) {
+            var num;
+            var item = {};
+            item.name = name;
+            item.qty = qty;
+            num = String(item.qty);
+            //rounded
+            if (num.length > 1) {
+                item.qty = item.qty;
+            } else
+            if (num.length == 1) {
+                item.qty = String(0) + item.qty;
+            }
 
+            list.push(item);
+            return list;
+        };
 
+        shop.list = list;
+        
 
-
-
-
-
+        shop.removeItem = function($index) {
+            list.splice($index, 1);
+            return list;
+        };
 
 
 
-var ShopController2 = shopApp.controller('ShopController2', ShopController2);
-
-
-function ShopController2() {
+    } //shop fn
 
 
 
+    function ShoppingListFactory() {
+    		
 
 
-}
-
-
-
-
-
+    }//ShoppingListFactory
 
 
 
+    
+
+
+    function ShopController2() {
+
+        var shop2 = this;
+        var list = [];
+
+        shop2.addItem = function(name, qty) {
+            var num;
+            var item = {};
+            item.name = name;
+            item.qty = qty;
+            num = String(item.qty);
+            //rounded
+            if (num.length > 1) {
+                item.qty = item.qty;
+            } else
+            if (num.length == 1) {
+                item.qty = String(0) + item.qty;
+            }
+
+            list.push(item);
+            return list;
+        };
+
+        shop2.list = list;
+
+        shop2.removeItem = function($index) {
+            list.splice($index, 1);
+            return list;
+        };
+
+    }
 
 
 
-
-
-
-
-
-
-
-
+})();
